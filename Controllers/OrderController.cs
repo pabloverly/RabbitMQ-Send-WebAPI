65a7d507f56fa9d1eaeab21981b206ffde70d070
pabloverly <pablopierre.pv@gmail.com>
2024-07-30 19:02:35 -0300

    push

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using WebApplication1.Domain;
+using System.Text.Json;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public IActionResult InsertOrder(Order order)
+        {
+            try
+            {
+                #region Gravar na Fila
+
+
+                var factory = new ConnectionFactory
+                {
+                    HostName = "191.252.191.68",
+                    Port = 5672, // Porta padrão para conexões AMQP (não HTTP)
+                    UserName = "guest", // Substitua pelo seu usuário do RabbitMQ
+                    Password = "guest" // Substitua pela sua senha do RabbitMQ
+                };
+
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
+
+                // Declarar a fila
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false, //1º true par a fila deve ser persistente (armazerzada em disco)
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                // Mensagem a ser enviada
+                string message = JsonSerializer.Serialize(order);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //2º Persistir a mensagem (fazer primeiro passo acima durable = true) fila fica 20% mais lenta 
+                // var basicProp = channel.CreateBasicProperties();
+                // basicProp.Persistent = true;
+
+                // Publicar a mensagem
+                channel.BasicPublish(exchange: string.Empty,
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Sent {message}");
+
+                Console.WriteLine(" Press [enter] to exit.");
+                //Console.ReadLine();
+                #endregion
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error inserting order");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+    }
+}
---
+++ Domain/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Domain
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public string Item { get; set; }
+        public float Price { get; set; }
+
+    }
+}
